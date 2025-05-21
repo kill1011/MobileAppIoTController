41929cdf5a0a32cd32ca551773f50e1cca416336
kill1011 <libronangelo406@gmail.com>
2025-05-21 22:43:47 +0800

    update model class and poject configuration

--- MobileAppIoTController/MauiProgram.cs
+++ MobileAppIoTController/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace MobileAppIoTController
 {
@@ -9,6 +10,7 @@ namespace MobileAppIoTController
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit() // Ensure CommunityToolkit is initialized
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -16,7 +18,7 @@ namespace MobileAppIoTController
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
---
+++ MobileAppIoTController/Models/TextMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MobileAppIoTController.Models
+{
+    public class TextMessage
+    {
+        public string? Value { get; set; }
+    }
+ }
+
