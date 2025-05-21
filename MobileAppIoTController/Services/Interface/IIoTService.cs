b389b21ad683d3008e895fb7f04e37a4a9853ea1
kill1011 <libronangelo406@gmail.com>
2025-05-21 22:54:36 +0800

    Add IoT service implementations with Firebase support

---
+++ MobileAppIoTController/Services/Interface/IIoTService.cs
@@ -0,0 +1,12 @@
+﻿using MobileAppIoTController.Models;
+namespace MobileAppIoTController.Services.Interfaces
+{
+    public interface IIoTService
+    {
+        event EventHandler<Temperature>? TemperatureDataChanged;
+        void StartListeningForTemperatureUpdates();
+        void StopListeningForTemperatureUpdates();
+        Task SaveTextMessageAsync(TextMessage textMessage);
+        Task SaveSwitchStateAsync(SwitchStates switchStates);
+    }
+}
