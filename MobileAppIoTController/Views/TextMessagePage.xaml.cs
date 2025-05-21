56c236efc93750a5339cef928ad47f06e34f536b
kill1011 <libronangelo406@gmail.com>
2025-05-22 00:14:08 +0800

    Add TextMessagePage with view model and tabbed navigation

---
+++ MobileAppIoTController/Views/TextMessagePage.xaml.cs
@@ -0,0 +1,14 @@
+using MobileAppIoTController.ViewModels;
+
+namespace MobileAppIoTController.Views;
+
+public partial class TextMessagePage : ContentPage
+{
+	public TextMessagePage(TextMessageViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+   
+}
